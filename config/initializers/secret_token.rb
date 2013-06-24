2d01095631cf493e80a4de229d9795c9cd920930
danott <thedanielott@gmail.com>
2013-06-24 14:04:07 -0700

    New Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scabbard::Application.config.secret_key_base = 'cd634e737d28351c9b9002d7bac0157d2913c21b45f418b6159f015fafdbe4762d35b46174039cfd489b8d160c6ff727429b39419ad4bbb6ea565a44332fb4fe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Scabbard::Application.config.session_store :cookie_store, key: '_scabbard_session'
