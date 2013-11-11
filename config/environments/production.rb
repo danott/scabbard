ab3d132a6a833d23ee2b571e455b08578c1ad5d2
Dan Ott <thedanielott@gmail.com>
2013-11-10 16:51:18 -0800

    Assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Scabbard::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( scabbard.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
